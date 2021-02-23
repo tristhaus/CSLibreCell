2bc1694b9ad22692882008c9e44028d0aacd6493
tristhaus <tristhaus@yahoo.de>
2021-02-23 20:31:29 +0100

    Add projects, PRNG and testing

---
+++ Core/Prng.cs
@@ -0,0 +1,28 @@
+﻿namespace Core
+{
+    /// <summary>
+    /// Implementation of the necessary, ancient Pseudo-Random Number Generator.
+    /// </summary>
+    public class Prng
+    {
+        private const uint Two16 = 65536; //google 2^16
+        private const uint Two31 = 2147483648; //google 2^31
+        private uint state;
+
+        public Prng()
+        {
+            this.state = 0;
+        }
+
+        internal void Initialize(uint newState)
+        {
+            this.state = newState;
+        }
+
+        internal uint GetNext()
+        {
+            this.state = (214013 * this.state + 2531011) % Two31;
+            return this.state / Two16;
+        }
+    }
+}
---
+++ CoreTest/PrngTest.cs
@@ -0,0 +1,69 @@
+﻿using Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoreTest
+{
+    /// <summary>
+    /// Tests for the Pseudo-Random Number Generator.
+    /// </summary>
+    [TestClass]
+    public class PrngTest
+    {
+        [TestMethod]
+        public void CreationOfCorrectNumbersShouldWork1()
+        {
+            // Arrange
+            var prng = new Prng();
+            prng.Initialize(1);
+
+            // values from original C++ LibreCell implementation
+            var reference = new List<uint> { 2745024 / 65536, 1210316419 / 65536 };
+
+            // Act
+            var result = new List<uint>(reference.Count);
+
+            for (int i = 0; i < reference.Count; i++)
+            {
+                result.Add(prng.GetNext());
+            }
+
+            // Assert
+            Assert.IsTrue(reference.SequenceEqual(result));
+        }
+
+        public void CreationOfCorrectNumbersShouldWork2()
+        {
+            // Arrange
+            var prng = new Prng();
+            prng.Initialize(0);
+
+            // values from Rosetta Code
+            var reference = new List<uint>
+            {
+                38,
+                7719,
+                21238,
+                2437,
+                8855,
+                11797,
+                8365,
+                32285,
+                10450,
+                30612
+            };
+
+            // Act
+            var result = new List<uint>(reference.Count);
+
+            for (int i = 0; i < reference.Count; i++)
+            {
+                result.Add(prng.GetNext());
+            }
+
+            // Assert
+            Assert.IsTrue(reference.SequenceEqual(result));
+        }
+    }
+}
