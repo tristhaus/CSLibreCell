5355741466296516d3d18691e9edd69886fe379f
tristhaus <tristhaus@yahoo.de>
2021-03-02 20:14:09 +0100

    Add keys and logic to make moves

--- CSLibreCell/Internal/Configuration.cs
+++ CSLibreCell/Internal/Configuration.cs
@@ -15,6 +15,44 @@ namespace CSLibreCell.Internal
                 internal static Key Help => Key.F1;
 
                 internal static Key Status => Key.F12;
+
+            }
+
+            internal static class Game
+            {
+                internal static Key Cancel => Key.Space;
+                
+                internal static Key Cell0 => (Key)'q';
+
+                internal static Key Cell1 => (Key)'w';
+                
+                internal static Key Cell2 => (Key)'e';
+                
+                internal static Key Cell3 => (Key)'r';
+                
+                internal static Key Column0 => (Key)'a';
+                
+                internal static Key Column1 => (Key)'s';
+                
+                internal static Key Column2 => (Key)'d';
+                
+                internal static Key Column3 => (Key)'f';
+                
+                internal static Key Column4 => (Key)'j';
+                
+                internal static Key Column5 => (Key)'k';
+                
+                internal static Key Column6 => (Key)'l';
+                
+                internal static Key Column7 => (Key)'ö';
+                
+                internal static Key Foundation0 => (Key)'u';
+                
+                internal static Key Foundation1 => (Key)'i';
+                
+                internal static Key Foundation2 => (Key)'o';
+             
+                internal static Key Foundation3 => (Key)'p';
             }
         }
     }
--- CSLibreCell/Program.cs
+++ CSLibreCell/Program.cs
@@ -9,6 +9,7 @@ namespace CSLibreCell
     {
         private static readonly Handler Handler = new Handler();
         private static Label GameLabel;
+        private static Location? Source = null;
 
         static void Main(string[] args)
         {
@@ -56,6 +57,23 @@ namespace CSLibreCell
 
         private static void Top_KeyPress(View.KeyEventEventArgs obj)
         {
+            void HandleLocation(Location location)
+            {
+                if (Source == null)
+                {
+                    Source = location;
+                }
+                else
+                {
+                    var refresh = Handler.ExecuteCommand(Handler.Command.Move((Location)Source, location));
+                    Source = null;
+                    if (refresh)
+                    {
+                        RefreshGame();
+                    }
+                }
+            }
+
             if (obj.KeyEvent.Key == Configuration.Keys.Menu.RandomGame)
             {
                 StartRandomGame();
@@ -71,7 +89,63 @@ namespace CSLibreCell
             else if (obj.KeyEvent.Key == Configuration.Keys.Menu.Status)
             {
                 ShowStatusDialog();
-            } //TODO: extend to playing system
+            }
+            else if (obj.KeyEvent.Key == Configuration.Keys.Game.Cancel)
+            {
+                Source = null;
+            }
+            else if (obj.KeyEvent.Key == Configuration.Keys.Game.Cell0)
+            {
+                HandleLocation(Location.Cell0);
+            }
+            else if (obj.KeyEvent.Key == Configuration.Keys.Game.Cell1)
+            {
+                HandleLocation(Location.Cell1);
+            }
+            else if (obj.KeyEvent.Key == Configuration.Keys.Game.Cell2)
+            {
+                HandleLocation(Location.Cell2);
+            }
+            else if (obj.KeyEvent.Key == Configuration.Keys.Game.Cell3)
+            {
+                HandleLocation(Location.Cell3);
+            }
+            else if (obj.KeyEvent.Key == Configuration.Keys.Game.Column0)
+            {
+                HandleLocation(Location.Column0);
+            }
+            else if (obj.KeyEvent.Key == Configuration.Keys.Game.Column1)
+            {
+                HandleLocation(Location.Column1);
+            }
+            else if (obj.KeyEvent.Key == Configuration.Keys.Game.Column2)
+            {
+                HandleLocation(Location.Column2);
+            }
+            else if (obj.KeyEvent.Key == Configuration.Keys.Game.Column3)
+            {
+                HandleLocation(Location.Column3);
+            }
+            else if (obj.KeyEvent.Key == Configuration.Keys.Game.Column4)
+            {
+                HandleLocation(Location.Column4);
+            }
+            else if (obj.KeyEvent.Key == Configuration.Keys.Game.Column5)
+            {
+                HandleLocation(Location.Column5);
+            }
+            else if (obj.KeyEvent.Key == Configuration.Keys.Game.Column6)
+            {
+                HandleLocation(Location.Column6);
+            }
+            else if (obj.KeyEvent.Key == Configuration.Keys.Game.Column7)
+            {
+                HandleLocation(Location.Column7);
+            }
+            else if (obj.KeyEvent.Key == Configuration.Keys.Game.Foundation0 || obj.KeyEvent.Key == Configuration.Keys.Game.Foundation1 || obj.KeyEvent.Key == Configuration.Keys.Game.Foundation2 || obj.KeyEvent.Key == Configuration.Keys.Game.Foundation3)
+            {
+                HandleLocation(Location.Foundation);
+            }
         }
 
         private static void StartRandomGame()
