b612fa75d0329b2991ab991f52d8ac9c3d83a168
tristhaus <tristhaus@yahoo.de>
2021-03-28 21:15:58 +0200

    Add copyright notices throughout

--- Core/Internal/IJourneyRepository.cs
+++ Core/Internal/IJourneyRepository.cs
@@ -1,4 +1,22 @@
-﻿namespace Core.Internal
+﻿/*
+ * This file is part of CSLibreCell.
+ * 
+ * CSLibreCell is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * CSLibreCell is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with CSLibreCell.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+namespace Core.Internal
 {
     /// <summary>
     /// Provides methods to read a journey from e.g. disk and write to the same source.
